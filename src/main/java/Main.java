9bd3ca44ea3587140200aa211ae12ecc0327ffb8
Horochov <horochov@10g.pl>
2019-05-13 17:59:39 +0200

    Updated terrain maker, implemented map shifting (for terrain generating purposes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,10 @@ import Terrain.Grid;
 import Terrain.TerrainMaker;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 import javax.swing.*;
 
 /*
@@ -29,6 +32,8 @@ public class Main
 	protected JPanel panel;
 	protected BufferedImage img;
 	protected Graphics imgG;
+	protected KeyListener keyListener;
+	protected TerrainMaker terrainMaker;
 	Grid grid;
 
 	public Main()
@@ -37,6 +42,8 @@ public class Main
 		//Prepare framebuffer
 		img=new BufferedImage(gridSize*gridWidth,gridSize*gridHeight, BufferedImage.TYPE_INT_ARGB);
 		imgG=img.getGraphics();
+
+		terrainMaker =  new TerrainMaker(gridWidth, gridHeight, gridSize);
 
 		panel = new JPanel()
 		{
@@ -63,8 +70,49 @@ public class Main
 		};
 		//TODO: frame double buffering
 
+		keyListener= new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e)
+			{
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e)
+			{
+//				System.out.println(e.getKeyCode());
+				switch (e.getKeyCode())
+				{
+					case 32:    //space
+						terrainMaker.setRandomSeed();
+						break;
+					case 38:    //up
+					case 40:    //down
+						terrainMaker.noiseOffset.modifyY(e.getKeyCode()-39 );
+						break;
+					case 37:
+					case 39:
+						terrainMaker.noiseOffset.modifyX(e.getKeyCode()-38);
+						break;
+				}
+				grid = terrainMaker.noiseBased(8);
+				grid.draw(imgG);
+				frame.repaint();
+
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e)
+			{
+//
+
+			}
+		};
+
 		frame = new JFrame();
 		frame.add(panel);
+		frame.addKeyListener(keyListener);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("Symulator zabijania zwierzat");
 		frame.setLocationByPlatform(true);
@@ -72,7 +120,7 @@ public class Main
 
 		frame.setVisible(true);
 //		paintGrid(imgG);
-		grid= new TerrainMaker(gridWidth, gridHeight, gridSize).noiseBased(8);
+		grid= terrainMaker.setRandomSeed().noiseBased(8);
 
 		grid.draw(imgG);
 		frame.repaint();
--- src/main/java/Terrain/TerrainMaker.java
+++ src/main/java/Terrain/TerrainMaker.java
@@ -10,25 +10,122 @@ public class TerrainMaker
 {
 	public Textures textures;
 	private Grid grid;
-	private OpenSimplexNoise noise;
+	private long seed;
+	public Offset noiseOffset;
+	public class Offset
+	{
+		int offsetX, offsetY;
+
+		public Offset(int x, int y)
+		{
+			offsetX=x;
+			offsetY=y;
+		}
+		public Offset()
+		{
+			offsetX=0;
+			offsetY=0;
+		}
+
+		public int modifyX(int dx)
+		{
+			return offsetX+=dx;
+		}
+		public int modifyY(int dy)
+		{
+			return offsetY+=dy;
+		}
+
+		public void modify(int dx, int dy)
+		{
+			offsetX+=dx;
+			offsetY+=dy;
+		}
+
+		public void setX(int offsetX)
+		{
+			this.offsetX = offsetX;
+		}
+
+		public void setY(int offsetY)
+		{
+			this.offsetY = offsetY;
+		}
+		public void set(int offsetX, int offsetY)
+		{
+			this.offsetX = offsetX;
+			this.offsetY = offsetY;
+		}
+
+		public void clear()
+		{
+			offsetX = 0;
+			offsetY = 0;
+		}
+
+		public int getX()
+		{
+			return offsetX;
+		}
+
+		public int getY()
+		{
+			return offsetY;
+		}
+	}
 
 	public TerrainMaker(int width, int height, int size)
 	{
 		grid = new Grid(width,height,size);
 		textures = new Textures();
-		Random r = new Random();
-		long seed = r.nextLong();
-		System.out.println("Map seed: "+seed);
-		noise = new OpenSimplexNoise(seed);
-
+		seed=0;
+		noiseOffset = new Offset();
+	}
+	public TerrainMaker(int width, int height, int size, long seed)
+	{
+		grid = new Grid(width,height,size);
+		textures = new Textures();
+		this.seed=seed;
+		noiseOffset = new Offset();
 	}
 
-	public Grid noiseBased(int divider)
+	public Grid getGrid()
 	{
+		return grid;
+	}
+
+
+
+
+
+	public TerrainMaker setSeed(long seed)
+	{
+		this.seed = seed;
+		return this;
+	}
+
+	public TerrainMaker setRandomSeed()
+	{
+		Random r = new Random();
+		seed = r.nextLong();
+		this.seed = seed;
+		return  this;
+	}
+
+	public long getSeed()
+	{
+		return seed;
+	}
+
+	public Grid noiseBased(int divider, long seed)
+	{
+//		System.out.println("Map seed: "+seed);
+		OpenSimplexNoise noise = new OpenSimplexNoise(seed);
+
 		for (int i = 0; i < grid.getWidth(); i++)
 			for (int j = 0; j < grid.getHeight(); j++)
 				try{
-					double rand = Math.abs(noise.eval((double)i/divider,(double)j/divider)) ;
+					double rand = Math.abs(noise.eval((double)(i+noiseOffset.getX())/divider,(double)(j+noiseOffset.getY())/divider)) ;
 					if(rand>0.6)
 						grid.tile(i,j).setTextures(textures.water);
 					else if(rand>0.5)
@@ -43,6 +140,14 @@ public class TerrainMaker
 
 		return grid;
 	}
+
+	public Grid noiseBased(int divider)
+	{
+		return noiseBased(divider,seed);
+	}
+
+
+
 
 	public Grid randomBased()
 	{
@@ -85,7 +190,9 @@ public class TerrainMaker
 			//water
 			try
 			{
-				water = new Image[]{new ImageIcon(getClass().getClassLoader().getResource("terrain/water.png")).getImage()};
+				water = new Image[]{
+						new ImageIcon(getClass().getClassLoader().getResource("terrain/water.png")).getImage(),
+				};
 			}
 			catch (Exception e)
 			{
@@ -95,7 +202,9 @@ public class TerrainMaker
 			//sand
 			try
 			{
-				sand = new Image[]{new ImageIcon(getClass().getClassLoader().getResource("terrain/sand.png")).getImage()};
+				sand = new Image[]{
+						new ImageIcon(getClass().getClassLoader().getResource("terrain/sand.png")).getImage(),
+				};
 			}
 			catch (Exception e)
 			{
@@ -108,7 +217,7 @@ public class TerrainMaker
 				grass = new Image[]{
 						new ImageIcon(getClass().getClassLoader().getResource("terrain/grass1.png")).getImage(),
 						new ImageIcon(getClass().getClassLoader().getResource("terrain/grass2.png")).getImage(),
-						new ImageIcon(getClass().getClassLoader().getResource("terrain/grass3.png")).getImage()
+						new ImageIcon(getClass().getClassLoader().getResource("terrain/grass3.png")).getImage(),
 				};
 			}
 			catch (Exception e)
